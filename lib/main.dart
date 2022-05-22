d18d2c9573dadd6e38b5892d40857bda9467e9c6
edwinmacalopu <khiwendz@gmail.com>
2022-05-21 21:41:29 -0500

    update nullsafety and flutter 3.0 :zap:

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
-void main() =>  
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: MyApp(),) 
-  );
+import 'package:latlong2/latlong.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ));
 
 class MyApp extends StatefulWidget {
   @override
@@ -17,39 +17,33 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("openstreetmap Flutter")
-        ,),
-        body: Container(
-          child:FlutterMap(
-        options: MapOptions(
-          center: LatLng(-12.069783, -77.034057),
-          zoom: 13.0
-          
-          ),
+        title: Text("openstreetmap Flutter"),
+      ),
+      body: Container(
+        child: FlutterMap(
+          options:
+              MapOptions(center: LatLng(-12.069783, -77.034057), zoom: 13.0),
           layers: [
             TileLayerOptions(
-              urlTemplate:'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
-            ),
-            MarkerLayerOptions(
-              markers: [
-                Marker(
-                  width:30.0,
+                urlTemplate:
+                    'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'),
+            MarkerLayerOptions(markers: [
+              Marker(
+                  width: 30.0,
                   height: 30.0,
                   point: LatLng(-12.069783, -77.034057),
-                  builder:(ctx)=>
-                  Container(
-                    child: Icon(Icons.location_on,color: Colors.blueAccent,size: 40,),
-                  )
-                  
-                   )
-              ]
-            )
+                  builder: (ctx) => Container(
+                          child: Container(
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.blueAccent,
+                          size: 40,
+                        ),
+                      )))
+            ])
           ],
-          
-      ),
-     
         ),
+      ),
     );
   }
 }
-
